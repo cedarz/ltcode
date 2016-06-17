13a5d2b240e5f313d6f2456ac8c455062d15e943
cedarz <dyingthesun@gmail.com>
2016-06-17 17:12:17 +0800

    add a table to the readme.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-#leetcode 
+#leetcode
+| Ã‚∫≈ | Ã‚ƒø | Solution|
+|.....|:......|.........|
+|1[Two Sum](https://leetcode.com/problems/two-sum)|[Solution 1](https://github.com/cedarz/ltcode/blob/master/1.cpp)|
+|2[Add Two Numbers](https://leetcode.com/problems/add-two-numbers)|[Solution 2](https://github.com/cedarz/ltcode/blob/master/2.cpp)|
+|3[Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters)|[Solution 3](https://github.com/cedarz/ltcode/blob/master/3.cpp)|
+|4[Median of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays)|[Solution 4](https://github.com/cedarz/ltcode/blob/master/4.cpp)|
+|5[Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring)|[Solution 5](https://github.com/cedarz/ltcode/blob/master/5.cpp)|
+|6[ZigZag Conversion](https://leetcode.com/problems/zigzag-conversion)|[Solution 6](https://github.com/cedarz/ltcode/blob/master/6.cpp)|
+|7[Reverse Integer](https://leetcode.com/problems/reverse-integer)|[Solution 7](https://github.com/cedarz/ltcode/blob/master/7.cpp)|
+|8[String to Integer (atoi)](https://leetcode.com/problems/string-to-integer-atoi)|[Solution 8](https://github.com/cedarz/ltcode/blob/master/8.cpp)|
+|9[Palindrome Number](https://leetcode.com/problems/palindrome-number)|[Solution 9](https://github.com/cedarz/ltcode/blob/master/9.cpp)|
+|10[Regular Expression Matching](https://leetcode.com/problems/regular-expression-matching)|[Solution 10](https://github.com/cedarz/ltcode/blob/master/10.cpp)|
---
+++ listprob.py
@@ -0,0 +1,66 @@
+# -*- coding: cp936 -*-
+#list the problems int the .md file
+
+import os
+import sys
+import re
+
+def read_file(file):
+    lf = []
+    f = open(file, 'r')
+    for line in f:
+        print line
+        item = ()
+
+        #id-num
+        match = re.match('[0-9]+', line)
+        #print match.span(), match.group()
+        item = item + (int(match.group()),)
+        #print item
+
+        #problem name
+        line = line[match.span()[1]:].strip()
+        match = re.match('[\D]+', line)
+        #print match.span(),
+        oname = match.group().strip()
+        name = match.group().strip().lower()
+        name = re.sub('[\(\)]', '', name)
+        name = name.replace(' ', '-')
+        #print "name : ", name
+        item = item + (oname, name)
+        
+
+        #problem correct
+        line = line[match.span()[1]:].strip()
+        match = re.match('[0-9]+\.[0-9]+', line)
+        #print match.span(), match.group().strip()
+        item = item + (match.group().strip() + '%',)
+
+        #easy hard
+        line = line[match.span()[1] + 1:].strip()
+        match = re.match('[a-zA-Z]+', line)
+        #print match.span(), match.group().strip()
+        item = item + (match.group().strip(),)
+
+        #print item
+        lf.append(item)
+    
+    lf.sort()
+    f.close()
+    print lf
+    return lf
+
+
+
+if __name__ == '__main__':
+    lf = read_file('problems.txt')
+
+    md = open('README.md', 'w')
+    md.write('#leetcode\n')
+    md.write('| Ã‚∫≈ | Ã‚ƒø | Solution|\n')
+    md.write('|.....|:......|.........|\n')
+    
+    for item in lf:
+        sline = '|' + str(item[0]) + '[' + item[1] + '](https://leetcode.com/problems/' + item[2] + ')|' + '[Solution ' + str(item[0]) + '](' + 'https://github.com/cedarz/ltcode/blob/master/' + str(item[0]) + '.cpp)|\n'
+        md.write(sline)
+    md.close()
