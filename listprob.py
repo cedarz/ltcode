a538fa8489669c20345a539c1a1f1dfc5edebcee
cedarz <dyingthesun@gmail.com>
2016-06-17 17:34:30 +0800

    encode error of the md

--- listprob.py
+++ listprob.py
@@ -4,7 +4,7 @@
 import os
 import sys
 import re
-
+import codecs
 def read_file(file):
     lf = []
     f = open(file, 'r')
@@ -55,10 +55,10 @@ def read_file(file):
 if __name__ == '__main__':
     lf = read_file('problems.txt')
 
-    md = open('README.md', 'w')
+    md = codecs.open('README.md', 'w', 'utf-8')
     md.write('#leetcode\n')
-    md.write('| # | 题目 | Solution|\n')
-    md.write('|----|:----|------|\n')
+    md.write(u'| # | 题目 | Solution|\n')
+    md.write('|-----|:-----|------|\n')
     
     for item in lf:
         sline = '|' + str(item[0]) + '|[' + item[1] + '](https://leetcode.com/problems/' + item[2] + ')|' + '[Solution ' + str(item[0]) + '](' + 'https://github.com/cedarz/ltcode/blob/master/' + str(item[0]) + '.cpp)|\n'
