c24a7a244c28c39374b9dbef169b9afc1a7f67f2
cedarz <dyingthesun@gmail.com>
2016-08-13 22:21:06 +0800

    41.cpp

--- 41.cpp
+++ 41.cpp
@@ -3,12 +3,11 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         int len = nums.size();
         for(int i = 0; i < len; ++i) {
-            int cur_i = i;
-            //int tmp = nums[i];
-            while(cur_i + 1 != nums[cur_i] && nums[cur_i] <= len && nums[cur_i] >= 1) {
-                int tmp = nums[nums[cur_i] - 1];
-                nums[nums[cur_i] - 1] = nums[cur_i];
-                cur_i = nums[cur_i] - 1;
+            //int cur_i = i;
+            int tmp = nums[i];
+            while(nums[tmp - 1] != tmp && tmp <= len && tmp >= 1) {
+                swap(tmp, nums[tmp - 1]);
+                //cur_i = tmp - 1;
             }
         }
         for(int i = 0; i < len; ++i) {
