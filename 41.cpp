1198870c4548545185738dc2a3322b57000fcd7b
cedarz <dyingthesun@gmail.com>
2016-08-14 19:50:32 +0800

    41.cpp

--- 41.cpp
+++ 41.cpp
@@ -5,7 +5,9 @@ public:
         for(int i = 0; i < len; ++i) {
             //int cur_i = i;
             int tmp = nums[i];
+            // for a num, its location should be at [num - 1]
             while(nums[tmp - 1] != tmp && tmp <= len && tmp >= 1) {
+                // get the num to be substituded
                 swap(tmp, nums[tmp - 1]);
                 //cur_i = tmp - 1;
             }
