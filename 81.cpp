ea81c8c405a22abb395449634c897bae3cc40580
cedarz <dyingthesun@gmail.com>
2017-03-12 16:12:47 +0800

    81 bst variation, i am in trouble

--- 81.cpp
+++ 81.cpp
@@ -1,7 +1,33 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        if(nums.empty()) return false;
         
+        int m = (low + high) / 2;
+        while(low <= high) {
+            if(target == nums[m]) {
+                return true;
+            }
+            if(nums[low] < nums[m]) {
+                if(target >= nums[low] && target < nums[m]) {
+                    high = m - 1;
+                } else {
+                    low = m + 1;
+                }
+            } else if(nums[low] > nums[m]) {
+                if(target > nums[m] && target <= nums[high]) {
+                    low = m + 1;
+                } else {
+                    high = m - 1;
+                }
+            } else {
+                high--;
+            }
+            m = (low + high) / 2;
+        } 
+        return false;
     }
 };
+
 
