e5f4686095e2b5129ac38204cf927d7a41373ff9
cedarz <dyingthesun@gmail.com>
2017-04-15 23:46:49 +0800

    88-90

--- 87.cpp
+++ 87.cpp
@@ -48,20 +48,18 @@ public:
 		
 		int len = s1.size();
         if(len == 1 && s1 != s2) return false;
+		string t1(s1), t2(s2);
+		sort(t1.begin(), t1.end());
+		sort(t2.begin(), t2.end());
+		if(t1 != t2) return false;
+		
         
 		for(int part = 1; part < len; ++ part) {
-			//int part = (len % 2 == 1) ? (len - 1) / 2 : len / 2;
-            //cout << part << endl;
-            //system("pause");
-			string t1 = s1.substr(0, part), t2 = s2.substr(0, part);
-			sort(t1.begin(), t1.end());
-			sort(t2.begin(), t2.end());
-			bool left = false, right = false;
-			if(t1 == t2) {
-				return  isScramble(s1.substr(0, part), s2.substr(0, part)) && isScramble(s1.substr(part, len - part), s2.substr(part, len - part));
-			} else {
-				return isScramble(s1.substr(0, len - part), s2.substr(part, len - part)) && isScramble(s1.substr(len - part, part), s2.substr(0, part));
-			}
+			//通过短路来剪枝
+			if(isScramble(s1.substr(0, part), s2.substr(0, part)) && isScramble(s1.substr(part, len - part), s2.substr(part, len - part))) return true;
+
+			if(isScramble(s1.substr(0, len - part), s2.substr(part, len - part)) && isScramble(s1.substr(len - part, part), s2.substr(0, part))) return true;
+
 		}
 		return false;
 	}
