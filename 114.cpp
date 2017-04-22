ed1ca0d2991477fc3fca51ccfb6cbb3210502bc8
cedarz <dyingthesun@gmail.com>
2017-04-22 23:24:27 +0800

    114

--- 114.cpp
+++ 114.cpp
@@ -10,6 +10,18 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
+		if(root == NULL) return;
         
+		else {
+			TreeNode * left = root->left, * right = root->right;
+			root->left = NULL;
+			root->right = left;
+			flatten(left);
+			TreeNode * r = root;
+			while(r->right != NULL) r = r->right;
+			r->right = right;
+			flatten(right);
+		}
     }
+
 };
