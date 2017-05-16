da7e7f8bb5272c4c6633cbb05b919813507ae643
cedarz <dyingthesun@gmail.com>
2017-05-16 15:07:05 +0800

    137

--- 137.cpp
+++ 137.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        
+        unordered_map<int, int> m;
+		for(int i = 0; i < nums.size(); ++i) {
+			if(m[nums[i]]) m[nums[i]]++;
+			else m[nums[i]] = 1;
+			
+			if(m[nums[i]] == 3) m.erase(nums[i]);
+		}
+		return m.begin()->first;
     }
 };
